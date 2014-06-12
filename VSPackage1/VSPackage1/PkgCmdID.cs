cf84487748718daa5495cf4e39634d7edf52ea55
Sarah Dong <t-xindo@microsoft.com>
2014-06-11 17:34:07 -0700

    cloned on sarah's computer and created a new package with one command

---
+++ VSPackage1/VSPackage1/PkgCmdID.cs
@@ -0,0 +1,13 @@
+﻿// PkgCmdID.cs
+// MUST match PkgCmdID.h
+using System;
+
+namespace Company.VSPackage1
+{
+    static class PkgCmdIDList
+    {
+        public const uint icecream2 =        0x100;
+
+
+    };
+}
---
+++ VSPackage1/VSPackage1/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("sarah1.0")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Company")]
+[assembly: AssemblyProduct("sarah1.0")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]   
+[assembly: ComVisible(false)]     
+[assembly: CLSCompliant(false)]
+[assembly: NeutralResourcesLanguage("en-US")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: InternalsVisibleTo("VSPackage1_IntegrationTests, PublicKey=00240000048000009400000006020000002400005253413100040000010001001924fb99bf0f9b3e4a2caaf026a7a3579474edcfeacd448cf91ece1e9b03de4f3a60b019f9e5ae19996c6f16d6387bff9d400b51298014b861e9cb8dc1cd3e7d35265841392e7a56e32ff0683a736320783d6cc2ff6ac8336e6a0042333d6027ee02e33bd27617f39960750ae1bfa022333bb2bf6b8add8706c2d879a28dbdee")]
+[assembly: InternalsVisibleTo("VSPackage1_UnitTests, PublicKey=00240000048000009400000006020000002400005253413100040000010001001924fb99bf0f9b3e4a2caaf026a7a3579474edcfeacd448cf91ece1e9b03de4f3a60b019f9e5ae19996c6f16d6387bff9d400b51298014b861e9cb8dc1cd3e7d35265841392e7a56e32ff0683a736320783d6cc2ff6ac8336e6a0042333d6027ee02e33bd27617f39960750ae1bfa022333bb2bf6b8add8706c2d879a28dbdee")]

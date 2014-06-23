ea00a00e2422aa4ed93ea63e7c2d9a7a628b2305
Justin Domingue <t-jusdom@microsoft.com>
2014-06-23 10:27:53 -0700

    Added untracked files : browser list icons and c# files

---
+++ VSMacros/GuidList.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace VSMacros
+{
+    internal static class GuidList
+    {
+        public const string GuidVSMacrosPkgString = "17f25bfa-a812-4b6a-a07e-7f73aa975f8b";
+        public const string GuidVSMacrosCmdSetString = "ad5b992c-e71a-4619-8d95-502b3b8de2c1";
+        public const string GuidToolWindowPersistanceString = "56fbfa32-c049-4fd5-9b54-39fcdf33629d";
+
+        public static readonly Guid GuidVSMacrosCmdSet = new Guid(GuidVSMacrosCmdSetString);
+    };
+}
---
+++ VSMacros/Tool Window Components/MacrosToolWindow.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Windows;
+using System.Runtime.InteropServices;
+using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.VisualStudio.Shell;
+using System.ComponentModel.Design; // for CommandID
+
+using VSMacros.Model;
+using VSMacros.Engines;
+
+
+namespace VSMacros
+{
+    [Guid("56fbfa32-c049-4fd5-9b54-39fcdf33629d")]
+    public class MacrosToolWindow : ToolWindowPane
+    {
+        public MacrosToolWindow() :
+            base(null)
+        {
+            this.Caption = Resources.ToolWindowTitle;
+            this.BitmapResourceID = 301;
+            this.BitmapIndex = 1;
+
+            // Instantiate Tool Window Toolbar
+            this.ToolBar = new CommandID(GuidList.GuidVSMacrosCmdSet, PkgCmdIDList.MacrosToolWindowToolbar);
+
+            // Initialize file system
+            if (!Directory.Exists(VSMacrosPackage.Current.MacroDirectory))
+                Directory.CreateDirectory(VSMacrosPackage.Current.MacroDirectory);
+
+            if (!File.Exists(Path.Combine(VSMacrosPackage.Current.MacroDirectory, "Current.js")))
+                File.Create(Path.Combine(VSMacrosPackage.Current.MacroDirectory, "Current.js"));
+
+            // Load Current macro
+            Manager.Instance.LoadCurrent();
+
+            string MacroDirectory = VSMacrosPackage.Current.MacroDirectory;
+            base.Content = new MacrosControl(new MacroFSNode(MacroDirectory));
+        }
+    }
+}

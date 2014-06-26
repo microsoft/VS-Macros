d2f08f647fc31e8c3dc243a02633b2be41034620
Grace Wannemacher <t-grawa@microsoft.com>
2014-06-26 16:22:06 -0700

    Revised the Executor class to conform with (most) StyleCop guidelines

--- VSMacros/Engines/Executor.cs
+++ VSMacros/Engines/Executor.cs
@@ -1,51 +1,104 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿//-----------------------------------------------------------------------
+// <copyright file="Executor.cs" company="Microsoft Corporation">
+//     Copyright Microsoft Corporation. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
 
 namespace VSMacros.Engines
 {
-    interface IExecutor
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.IO;
+    using System.Linq;
+    using System.Reflection;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Windows;
+
+    /// <summary>
+    /// Exposes the execution engine.
+    /// </summary>
+    internal interface IExecutor
     {
-        void InitializeAndRunEngine();
-        void StartExecution(Stream macro, int times);
-        void StopExecution();
+        /// <summary>
+        /// Informs subscribers of an error during execution.
+        /// </summary>
         event EventHandler OnError;
+
+        /// <summary>
+        /// Informs subscribers of success after execution.
+        /// </summary>
         event EventHandler OnSuccess;
+
+        /// <summary>
+        /// blah blah blah
+        /// </summary>
+        void InitializeAndRunEngine();
+
+        /// <summary>
+        /// blah blah blah
+        /// <param name="macro">Name of macro.</param>
+        /// <param name="times">Times to be executed.</param>
+        /// </summary>
+        void StartExecution(StreamReader macro, int times);
+
+        /// <summary>
+        /// blah blah blah
+        /// </summary>
+        void StopExecution();
     }
-        
+
+    /// <summary>
+    /// blah blah blah
+    /// </summary>   
     internal class Executor : IExecutor
     {
-        Process executionEngine;
+        /// <summary>
+        /// The execution engine.
+        /// </summary>
+        private Process executionEngine;
 
+        /// <summary>
+        /// Informs subscribers of an error during execution.
+        /// </summary>
+        public event EventHandler OnError;
+
+        /// <summary>
+        /// Informs subscribers of success after execution.
+        /// </summary>
+        public event EventHandler OnSuccess;
+
+        /// <summary>
+        /// Initializes the engine and then runs the macro script.
+        /// This method will be removed after IPC is implemented.
+        /// </summary>
         public void InitializeAndRunEngine()
         {
             var currentProcess = Process.GetCurrentProcess();
             var processName = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "ExecutionEngine.exe");
-            executionEngine = new Process();
+            this.executionEngine = new Process();
 
-            executionEngine.StartInfo.FileName = processName;
-            executionEngine.StartInfo.Arguments = Process.GetCurrentProcess().Id.ToString();
-            executionEngine.Start();
+            this.executionEngine.StartInfo.FileName = processName;
+            this.executionEngine.StartInfo.Arguments = Process.GetCurrentProcess().Id.ToString();
+            this.executionEngine.Start();
         }
 
-        public void StartExecution(Stream macro, int times)
+        /// <summary>
+        /// Will run the macro file.
+        /// </summary>
+        public void StartExecution(StreamReader macro, int times)
         {
-            //throw new NotImplementedException();
+            // throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Will stop the currently executing macro file.
+        /// We are considering removing this.
+        /// </summary>
         public void StopExecution()
         {
-            //throw new NotImplementedException();
+            // throw new NotImplementedException();
         }
-
-        public event EventHandler OnError;
-
-        public event EventHandler OnSuccess;
     }
 }

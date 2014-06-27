9b7a1e597838f04067ec30edede8b8809c9bca12
Grace Wannemacher <t-grawa@microsoft.com>
2014-06-27 11:19:49 -0700

    A stub to create a StreamReader for the execution engine.  This substitutes for the StreamReader that management will pass in

---
+++ VSMacros/Stubs/MacroReader.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicrosoftCorporation.VSMacros.Stubs
+{
+    class MacroReader
+    {
+        string name = "currentScript";
+
+        internal string AppendExtension(string name)
+        {
+            return name + ".txt";
+        }
+
+        public StreamReader CreateMacroStreamReader()
+        {
+            var path = AppendExtension(this.name);
+            using (StreamWriter sw = new StreamWriter(path))
+            {
+                sw.WriteLine("dte.ExecuteCommand('File.NewFile');");
+            }
+
+            return new StreamReader(path);
+        }
+    }
+}

5da2208c78d5f54c082ddbd59a87bcd856b24022
Justin Domingue <t-jusdom@microsoft.com>
2014-07-03 19:28:39 -0700

    Hewlett-Packaard Company again

--- ManagementTest/Properties/AssemblyInfo.cs
+++ ManagementTest/Properties/AssemblyInfo.cs
@@ -14,9 +14,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("ManagementTest")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Hewlett-Packard Company")]
+[assembly: AssemblyCompany("Microsoft Corporation")]
 [assembly: AssemblyProduct("ManagementTest")]
-[assembly: AssemblyCopyright("Copyright © Hewlett-Packard Company 2014")]
+[assembly: AssemblyCopyright("Copyright © Microsoft Corporation 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 

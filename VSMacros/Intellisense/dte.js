7dcc89cf781366b5f9ee93b90db627413e3b4f05
Justin Domingue <t-jusdom@microsoft.com>
2014-07-10 15:33:10 -0700

    Improved js dte coverage

--- VSMacros/Intellisense/dte.js
+++ VSMacros/Intellisense/dte.js
@@ -1,63 +1,78 @@
-// The top-level object in the Visual Studio automation object model.
-var dte = new function () {
+//-----------------------------------------------------------------------
+// <summary>
+//      Defines an object dte used to provide context to the 
+//      javascript intellisense.
+// </summary>
+// <copyright file="dte.js" company="Microsoft Corporation">
+//     Copyright Microsoft Corporation. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
 
-    // Gets the active document.
-    this.ActiveDocument = new function () {
-
+// Helper variable
+var _dte = {
+    // Represents a document in the environment open for editing.
+    Document: new function () {
         // Gets the collection containing the EnvDTE.Document object.
-        this.Collection;
+        this.Collection = new _DTEDocument;
 
         // Gets the top-level extensibility object.
-        this.DTE;
+        this.DTE = "";
 
         // Gets the Extender category ID (CATID) for the object.
-        this.ExtenderCATID;
+        this.ExtenderCATID = "";
 
         // Gets a list of available Extenders for the object.
-        this.ExtenderNames;
+        this.ExtenderNames = "";
 
         // Gets the full path and name of the object's file.
-        this.FullName;
+        this.FullName = "";
 
         // Microsoft Internal Use Only.
-        this.IndentSize;
+        this.IndentSize = "";
 
         // Gets a GUID string indicating the kind or type of the object.
-        this.Kind;
+        this.Kind = "";
 
         // Microsoft Internal Use Only.
-        this.Language;
+        this.Language = "";
 
         // Gets the name of the EnvDTE.Document.
-        this.Name;
+        this.Name = "";
 
         // Gets the path, without file name, for the directory containing the document.
-        this.Path;
+        this.Path = "";
+
         // Gets the EnvDTE.ProjectItem object associated with the EnvDTE.Document object.
-        this.ProjectItem;
+        this.ProjectItem = "";
+
         // Microsoft Internal Use Only.
-        this.ReadOnly;
+        this.ReadOnly = "";
 
         // Returns true if the object has not been modified since last being saved or opened.
-        this.Saved;
+        this.Saved = "";
 
         // Gets an object representing the current selection on the EnvDTE.Document.
-        this.Selection;
+        this.Selection = "";
 
         // Microsoft Internal Use Only.
-        this.TabSize;
+        this.TabSize = "";
 
         // Microsoft Internal Use Only.
-        this.Type;
+        this.Type = "";
 
         // Gets a EnvDTE.Windows collection containing the windows that display in the object.
-        this.Windows;
+        this.Windows = "";
 
         // Moves the focus to the current item.
         this.Activate = function () { }
 
         // Microsoft Internal Use Only.
         this.ClearBookmarks = function () { }
+
+        this.Close = function () {
+            /// <summary>Closes the open document and optionally saves it, or closes and destroys the window.</summary>
+            /// <params>Optional. A vsSaveChanges constant that determines whether to save an item or items (vsSaveChanges.vsSaveChangesNo, vsSaveChanges.vsSaveChangesPrompt, vsSaveChanges.vsSaveChangesYes.</params>
+        }
 
         // Closes the open document and optionally saves it, or closes and destroys the
         this.NewWindow = function () { }
@@ -68,144 +83,194 @@ var dte = new function () {
         // Re-executes the last action that was undone by the EnvDTE.Document.Undo method
         this.Redo = function () { }
 
+        // Saves the document.
+        this.Save = function () { }
+
         // Microsoft Internal Use Only.
-        this.Undo = function () { 
+        this.Undo = function () {
+        }
+    },
+
+    // Contains all Document objects in the environment, each representing an open document.
+    Documents: new function () {
+        // Gets a value indicating the number of objects in the EnvDTE.Documents collection.
+        this.Count = "";
+
+        // Gets the top-level extensibility object.
+        this.DTE = "";
+
+        // Gets the immediate parent object of a EnvDTE.Documents collection.
+        this.Parent = "";
+
+        /// <summary>Closes all open documents in the environment and optionally saves them.</summary>
+        // <param name="Save">Optional. A EnvDTE.vsSaveChanges constant representing how to react to changes made to documents</param>
+        this.CloseAll = function (Save) { }
+
+        // Returns an enumerator for items in the collection.
+        this.GetEnumerator = function () { }
+
+        // Returns an indexed member of a EnvDTE.Documents collection.
+        // <param name="index">Required. The index of the item to return.</param>
+        this.Item = function (index) {
+            return _dte.Document;
+        }
+
+        // Saves all documents currently open in the environment.
+        this.SaveAll = function () { }
+
+    },
+
+    ActiveWindow: new function () { },
+
+    AddIns: new function () { },
+
+    CommandBars: new function () { }
+}
+
+// The top-level object in the Visual Studio automation object model.
+var dte = new function () {
+    // Gets the active document.
+    this.ActiveDocument = new function () {
+        return _dte.Document;
     }
 
     // Gets an array of currently selected projects.
-    this.ActiveSolutionProjects = function () {
-    }
+    this.ActiveSolutionProjects = "";
 
     // Returns the currently active window, or the top-most window if no others are active.
-    this.ActiveWindow = function () {
+    this.ActiveWindow = new function () {
+        return _dte.ActiveWindow;
     }
 
     // Gets the EnvDTE.AddIns collection, which contains all currently available Add-ins.
-    this.AddIns = function () {
+    this.AddIns = new function () {
+        return _dte.AddIns;
     }
 
     // Gets a reference to the development environment's command bars.
-    this.CommandBars = function () {
+    this.CommandBars = new function () {
+        return _dte.CommandBars;
     }
 
     // Gets a string representing the command line arguments.
-    this.CommandLineArguments = function () {
+    this.CommandLineArguments = new function () {
     }
 
     // Returns the EnvDTE.Commands collection.
-    this.Commands = function () {
+    this.Commands = new function () {
     }
 
     // Gets a EnvDTE.ContextAttributes collection which allows automation clients to add new attributes to the current selected items in the Dynamic Help window and provide contextual help for the additional attributes.
-    this.ContextAttributes = function () {
+    this.ContextAttributes = new function () {
     }
 
     // Gets the debugger objects.
-    this.Debugger = function () {
+    this.Debugger = new function () {
     }
 
     // Gets the display mode, either MDI or Tabbed Documents.
-    this.DisplayMode = function () {
+    this.DisplayMode = new function () {
     }
 
     // Gets the collection of open documents in the development environment.
-    this.Documents = function () {
+    this.Documents = new function () {
+        return _dte.Documents;
     }
 
     // Gets the top-level extensibility object.
-    this.DTE = function () {
+    this.DTE = new function () {
     }
 
     // Gets a description of the edition of the environment.
-    this.Edition = function () {
+    this.Edition = new function () {
     }
 
     // Gets a reference to the EnvDTE.Events object.
-    this.Events = function () {
+    this.Events = new function () {
     }
 
     // Microsoft Internal Use Only.
-    this.FileName = function () {
+    this.FileName = new function () {
     }
 
     // Gets the EnvDTE.Find object that represents global text find operations.
-    this.Find = function () {
+    this.Find = new function () {
     }
 
     // Gets the full path and name of the object's file.
-    this.FullName = function () {
+    this.FullName = new function () {
     }
 
     // Gets the EnvDTE.Globals object that contains Add-in values that may be saved in the solution (.sln) file, the project file, or in the user's profile data.
-    this.Globals = function () {
+    this.Globals = new function () {
     }
 
     // Gets the EnvDTE.ItemOperations object.
-    this.ItemOperations = function () {
+    this.ItemOperations = new function () {
     }
 
     // Gets the ID of the locale in which the development environment is running.
-    this.LocaleID = function () {
+    this.LocaleID = new function () {
     }
 
     // Gets a EnvDTE.Window object representing the main development environment window.
-    this.MainWindow = function () {
+    this.MainWindow = new function () {
     }
 
     // Gets the mode of the development environment, either debug or design.
-    this.Mode = function () {
+    this.Mode = new function () {
     }
 
     // Sets or gets the name of the EnvDTE._DTE object.
-    this.Name = function () {
+    this.Name = new function () {
     }
 
     // Gets the EnvDTE.ObjectExtenders object.
-    this.ObjectExtenders = function () {
+    this.ObjectExtenders = new function () {
     }
 
     // Gets a string with the path to the root of the Visual Studio registry settings.
-    this.RegistryRoot = function () {
+    this.RegistryRoot = new function () {
     }
 
     // Gets a collection containing the items currently selected in the environment.
-    this.SelectedItems = function () {
+    this.SelectedItems = new function () {
     }
 
     // Gets the EnvDTE.Solution object that represents all open projects in the current instance of the environment and allows access to the build objects.
-    this.Solution = function () {
+    this.Solution = new function () {
     }
 
     // Gets a EnvDTE.SourceControl object that allows you to manipulate the source code control state of the file behind the object.
-    this.SourceControl = function () {
+    this.SourceControl = new function () {
     }
 
     // Gets the EnvDTE.StatusBar object, representing the status bar on the main development environment window.
-    this.StatusBar = function () {
+    this.StatusBar = new function () {
     }
 
     // Gets or sets whether UI should be displayed during the execution of automation code.
-    this.SuppressUI = function () {
+    this.SuppressUI = new function () {
     }
 
     // Gets the global EnvDTE.UndoContext object.
-    this.UndoContext = function () {
+    this.UndoContext = new function () {
     }
 
     // Sets or gets a value indicating whether the environment was launched by a user or by automation.
-    this.UserControl = function () {
+    this.UserControl = new function () {
     }
 
     // Gets the host application's version number.
-    this.Version = function () {
+    this.Version = new function () {
     }
 
     // Gets the EnvDTE.WindowConfigurations collection, representing all available window configurations.
-    this.WindowConfigurations = function () {
+    this.WindowConfigurations = new function () {
     }
 
     // Gets a EnvDTE.Windows collection containing the windows that display in the object.
-    this.Windows = function () {
+    this.Windows = new function () {
     }
 
     this.ExecuteCommand = function (CommandName, CommandArgs) {

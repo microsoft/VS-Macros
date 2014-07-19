8b35bc169b0a7cbb8f5ae5d1b27d6d9a6df63af4
Justin Domingue <t-jusdom@microsoft.com>
2014-07-18 17:47:26 -0700

    Delete now uses SHFileOperation

--- VSMacros/Engines/Manager.cs
+++ VSMacros/Engines/Manager.cs
@@ -15,6 +15,7 @@ using VSMacros.Dialogs;
 using VSMacros.Interfaces;
 using VSMacros.Models;
 using Microsoft.VisualBasic.FileIO;
+using System.Runtime.InteropServices;
 
 namespace VSMacros.Engines
 {
@@ -592,6 +593,42 @@ namespace VSMacros.Engines
                 out result));
 
             return (VSConstants.MessageBoxResult)result;
+        }
+
+        #endregion
+
+        #region
+
+        private const int FO_DELETE = 0x0003;
+        private const int FOF_ALLOWUNDO = 0x0040;           // Preserve undo information, if possible. 
+        private const int FOF_NOCONFIRMATION = 0x0010;      // Show no confirmation dialog box to the user
+
+        // Struct which contains information that the SHFileOperation function uses to perform file operations. 
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+        public struct SHFILEOPSTRUCT
+        {
+            public IntPtr hwnd;
+            [MarshalAs(UnmanagedType.U4)]
+            public int wFunc;
+            public string pFrom;
+            public string pTo;
+            public short fFlags;
+            [MarshalAs(UnmanagedType.Bool)]
+            public bool fAnyOperationsAborted;
+            public IntPtr hNameMappings;
+            public string lpszProgressTitle;
+        }
+
+        [DllImport("shell32.dll", CharSet = CharSet.Auto)]
+        static extern int SHFileOperation(ref SHFILEOPSTRUCT FileOp);
+
+        public static void DeleteFileOrFolder(string path)
+        {
+            SHFILEOPSTRUCT fileop = new SHFILEOPSTRUCT();
+            fileop.wFunc = FO_DELETE;
+            fileop.pFrom = path + '\0' + '\0';
+            fileop.fFlags = FOF_ALLOWUNDO | FOF_NOCONFIRMATION;
+            SHFileOperation(ref fileop);
         }
 
         #endregion

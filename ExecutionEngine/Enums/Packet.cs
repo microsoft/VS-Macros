825119c6a81208fa2e708b672013be45da2c3605
Grace Wannemacher <t-grawa@microsoft.com>
2014-07-18 17:59:16 -0700

    Add untracked files (sorry)

---
+++ ExecutionEngine/Enums/Packet.cs
@@ -0,0 +1,42 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Packet.cs" company="Microsoft Corporation">
+//     Copyright Microsoft Corporation. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExecutionEngine.Enums
+{
+    internal enum Packet
+    {
+        /// <summary>
+        /// Contains information to parse a file path.
+        /// </summary>
+        FilePath = 0,
+
+        /// <summary>
+        /// Sends a request to close the engine.
+        /// </summary>
+        Close = 1,
+
+        /// <summary>
+        /// Notifies subscribers of success completion of macro.
+        /// </summary>
+        Success = 2,
+
+        /// <summary>
+        /// Notifies subscribers of an error upon running the script.
+        /// </summary>
+        ScriptError = 3,
+
+        /// <summary>
+        /// Notifies subscribers of internal VS error.
+        /// </summary>
+        CriticalError = 4
+    }
+}

b6eb287d0696f557a81aa9ffa798f99fa3f64a41
Grace Wannemacher <t-grawa@microsoft.com>
2014-07-28 15:51:35 -0700

    Update some unit tests

--- ExecutionTest/EngineTests.cs
+++ ExecutionTest/EngineTests.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ExecutionEngine;
+using VSMacros.ExecutionEngine;
 
 namespace ExecutionTest
 {
@@ -21,5 +24,17 @@ namespace ExecutionTest
         // Make sure site is connected
 
         #endregion
+
+        //private Engine engine;
+
+        [TestMethod]
+        public void Constructor_InitializesEngine()
+        {
+            //int pid = 0;
+            //string version = "hello";
+            //var engine = new Engine(pid, version);
+            //Assert.AreEqual(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "Test.js"), this.fileNode.FullPath);
+            //Assert.IsFalse(this.fileNode.IsEditable);
+        }
     }
 }
--- ExecutionTest/PipeTests.cs
+++ ExecutionTest/PipeTests.cs
@@ -1,24 +1,92 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO.Pipes;
 using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VisualStudio.Macros.ExecutionEngine.Pipes;
+using VSMacros.ExecutionEngine.Pipes;
+using VSMacros.Pipes;
 
 namespace ExecutionTest
 {
     class PipeTests
     {
-        #region Initialization
+        private NamedPipeServerStream serverStream;
+        private NamedPipeClientStream clientStream;
+        private BinaryFormatter serializer;
 
-        // Make sure guid is same for both Pipe and Client
+        #region Initialization
+        [TestMethod]
+        public void Constructor_InitializesPipes()
+        {
+            Server.InitializeServer();
+            var guid = Server.Guid;
+            Client.InitializePipeClientStream(guid);
+
+            serverStream = Server.ServerStream;
+            clientStream = Client.ClientStream;
+
+            Assert.IsTrue(serverStream.IsConnected);
+            Assert.IsTrue(clientStream.IsConnected);
+        }
+
+        [TestMethod]
+        public void Constructor_InitializesSerializer()
+        {
+            serializer = new BinaryFormatter();
+
+            Assert.IsTrue(serverStream.IsConnected);
+            Assert.IsTrue(clientStream.IsConnected);
+        }
 
         #endregion
 
         #region Message Passing
 
-        // Make sure message is encoded correctly
+        [TestMethod]
+        public void ExchangingSuccessMessage_MessageSuccessfullyExchanged()
+        {
+            var sentPacketType = PacketType.Success;
+            serializer.Serialize(clientStream, sentPacketType);
+            var receivedType = (PacketType)serializer.Deserialize(serverStream);
+            
+            Assert.AreEqual(sentPacketType, receivedType);
+        }
 
-        // Make sure message is decoded correctly
+        [TestMethod]
+        public void ExchangingFilePathSimple_FilePathSuccessfullyExchanged()
+        {
+            var sentFilePath = new FilePath();
+            sentFilePath.Iterations = 1;
+            sentFilePath.Path = @"C\Windows\file path";
+
+            serializer.Serialize(clientStream, sentFilePath);
+            var receivedFilePath = (FilePath)serializer.Deserialize(serverStream);
+
+            Assert.AreEqual(sentFilePath.Iterations, receivedFilePath.Iterations);
+            Assert.AreEqual(sentFilePath.Path, receivedFilePath.Path);
+        }
+
+        public void ExchangingFilePathWithTypeIndicator_FilePathSuccessfullyExchanged()
+        {
+            var sentFilePathType = PacketType.FilePath;
+            serializer.Serialize(clientStream, sentFilePathType);
+
+            var sentFilePath = new FilePath();
+            sentFilePath.Iterations = 1;
+            sentFilePath.Path = @"C\Windows\file path";
+            serializer.Serialize(clientStream, sentFilePath);
+
+            var receivedFilePathType = (PacketType)serializer.Deserialize(serverStream);
+            var receivedFilePath = (FilePath)serializer.Deserialize(serverStream);
+
+            Assert.AreEqual(sentFilePathType, receivedFilePathType);
+            Assert.AreEqual(sentFilePath.Iterations, receivedFilePath.Iterations);
+            Assert.AreEqual(sentFilePath.Path, receivedFilePath.Path);
+        }
 
         #endregion
     }

c341993f5a98fb3a9fa9a37f1a2c636e0d865b77
Grace Wannemacher <t-grawa@microsoft.com>
2014-07-28 13:23:35 -0700

    Starting work on unit tests

---
+++ ExecutionTest/EngineTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExecutionTest
+{
+    class EngineTests
+    {
+        #region Initialization
+
+        // Make sure dte object is not null
+
+        // Make sure command helper object is not null
+
+        #endregion
+
+        #region Site
+
+        // Make sure site is connected
+
+        #endregion
+    }
+}
---
+++ ExecutionTest/PipeTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExecutionTest
+{
+    class PipeTests
+    {
+        #region Initialization
+
+        // Make sure guid is same for both Pipe and Client
+
+        #endregion
+
+        #region Message Passing
+
+        // Make sure message is encoded correctly
+
+        // Make sure message is decoded correctly
+
+        #endregion
+    }
+}

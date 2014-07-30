60bff011cea543b1c0632e750e97b7b0c2280d92
Grace Wannemacher <t-grawa@microsoft.com>
2014-07-29 20:01:37 -0700

    one serializer for entire Client class

--- ExecutionEngine/Pipes/Client.cs
+++ ExecutionEngine/Pipes/Client.cs
@@ -14,53 +14,55 @@ namespace VSMacros.ExecutionEngine.Pipes
     public static class Client
     {
         public static NamedPipeClientStream ClientStream;
+        private static BinaryFormatter serializer;
+
         public static void InitializePipeClientStream(Guid guid)
         {
             var timeoutInMilliseconds = 12000;
             Client.ClientStream = new NamedPipeClientStream(".", guid.ToString(), PipeDirection.InOut, PipeOptions.Asynchronous);
             Client.ClientStream.Connect(timeoutInMilliseconds);
+
+            Client.serializer = new BinaryFormatter();
+            Client.serializer.Binder = new BinderHelper();
         }
 
         public static void ShutDownServer(NamedPipeClientStream clientStream)
         {
             var packetType = PacketType.Close;
-            BinaryFormatter formatter = new BinaryFormatter();
-            formatter.Serialize(Client.ClientStream, packetType);
+            Client.serializer.Serialize(Client.ClientStream, packetType);
         }
 
         internal static void SendScriptError(uint modifiedLineNumber, int column, string source, string description)
         {
             var type = PacketType.ScriptError;
-            BinaryFormatter formatter = new BinaryFormatter();
-            formatter.Serialize(Client.ClientStream, type);
+            Client.serializer.Serialize(Client.ClientStream, type);
 
             var scriptError = new ScriptError();
             scriptError.LineNumber = (int)modifiedLineNumber;
             scriptError.Column = column;
             scriptError.Source = source;
             scriptError.Description = description;
-            formatter.Serialize(Client.ClientStream, scriptError);
+            Client.serializer.Serialize(Client.ClientStream, scriptError);
         }
 
         internal static void SendCriticalError(string message, string source, string stackTrace, string targetSite)
         {
             var type = PacketType.CriticalError;
             BinaryFormatter formatter = new BinaryFormatter();
-            formatter.Serialize(Client.ClientStream, type);
+            Client.serializer.Serialize(Client.ClientStream, type);
 
             var criticalError = new CriticalError();
             criticalError.Message = message;
             criticalError.Source = source;
             criticalError.StackTrace = stackTrace;
             criticalError.TargetSite = targetSite;
-            formatter.Serialize(Client.ClientStream, criticalError);
+            Client.serializer.Serialize(Client.ClientStream, criticalError);
         }
 
         internal static void SendSuccessMessage()
         {
             var packetType = PacketType.Success;
-            BinaryFormatter formatter = new BinaryFormatter();
-            formatter.Serialize(Client.ClientStream, packetType);
+            Client.serializer.Serialize(Client.ClientStream, packetType);
         }
     }
 }

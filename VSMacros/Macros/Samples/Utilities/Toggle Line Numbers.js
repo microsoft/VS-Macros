2a7686390710484a2031a2283c27c06e837aabad
Justin Clareburt <justcla@microsoft.com>
2017-08-11 17:14:02 -0700

    Added error handling to ToggleLineNumbers

--- VSMacros/Macros/Samples/Utilities/Toggle Line Numbers.js
+++ VSMacros/Macros/Samples/Utilities/Toggle Line Numbers.js
@@ -3,6 +3,10 @@
 var lang = ["Basic", "Plaintext", "CSharp", "HTML", "C/C++", "XML", "JavaScript"];
 
 for (var i = 0; i < lang.length; i++) {
-    var currentValue = dte.Properties("TextEditor", lang[i]).Item("ShowLineNumbers").Value;
-    dte.Properties("TextEditor", lang[i]).Item("ShowLineNumbers").Value = !currentValue
+    try {
+        var currentValue = dte.Properties("TextEditor", lang[i]).Item("ShowLineNumbers").Value;
+        dte.Properties("TextEditor", lang[i]).Item("ShowLineNumbers").Value = !currentValue
+    } catch(err) {
+        // Swallow exception and move on
+    }
 }
